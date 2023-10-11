bde9849803d4368fa58c400943fe4b8be9b6cd31
springevidence <exshipbuilder1@gmail.com>
2023-10-11 15:26:48 +0300

    added icons in footer

--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import style from './Footer.module.scss'
+import telegramIcon from './../assets/icons/telegram.svg'
+import instagramIcon from './../assets/icons/instagram.svg'
+import linkedinIcon from './../assets/icons/linkedin.png'
 
 const Footer = () => {
     return (
@@ -7,10 +10,22 @@ const Footer = () => {
             <div className={style.container}>
                 <span className={style.name}>Тюрина Виктория</span>
                 <div className={style.socialBlock}>
-                    <div className={style.socialItem}></div>
-                    <div className={style.socialItem}></div>
-                    <div className={style.socialItem}></div>
-                    <div className={style.socialItem}></div>
+                    <div className={style.socialItem}>
+                        <a href={''}>
+                            <img src={telegramIcon} alt=""/>
+                        </a>
+                    </div>
+                    <div className={style.socialItem}>
+                        <a href={''}>
+                            <img src={instagramIcon} alt=""/>
+                        </a>
+                    </div>
+                    <div className={style.socialItem}>
+                        <a href={''}>
+                            <img src={linkedinIcon} alt=""/>
+                        </a>
+                    </div>
+                    {/*<div className={style.socialItem}></div>*/}
                 </div>
                 <span className={style.copyrightTitle}>© 2023 Все права защищены</span>
             </div>
